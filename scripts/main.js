5aefe1127aa7ee3374348eb91669a2a7472d7dc2
zy <zeng.yu.1993@gmail.com>
2020-05-09 17:28:50 +0800

    [ADD] 添加creep生成和寻路逻辑

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,26 @@
 module.exports.loop = function () {
-    console.log(sayHello('world!'));
+    var spawn = Game.spawns["Spawn1"];
+    if (spawn.store.energy > 50) {
+        spawn.spawnCreep([WORK, MOVE, CARRY], "miner");
+    }
+    for (var name_1 in Game.creeps) {
+        runCreep(Game.creeps[name_1]);
+    }
 };
-function sayHello(str) {
-    return 'hello' + str;
+function runCreep(creep) {
+    var source = creep.pos.findClosestByPath(FIND_SOURCES);
+    if (source == null) {
+        creep.say("no source");
+        return;
+    }
+    if (creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
+        if (creep.transfer(Game.spawns["Spawn1"], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(Game.spawns["Spawn1"].pos);
+            return;
+        }
+    }
+    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source.pos);
+        return;
+    }
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,7 +1,30 @@
 module.exports.loop = function(): void {
-    console.log(sayHello('world!'));
+    let spawn = Game.spawns["Spawn1"];
+    if (spawn.store.energy > 50) {
+        spawn.spawnCreep([WORK, MOVE, CARRY], "miner");
+    }
+
+    for (let name in Game.creeps) {
+        runCreep(Game.creeps[name]);
+    }
 }
 
-function sayHello(str: string): string {
-    return 'hello' + str;
+function runCreep(creep: Creep): void {
+    let source = creep.pos.findClosestByPath(FIND_SOURCES);
+    if (source == null) {
+        creep.say("no source");
+        return;
+    }
+
+    if (creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
+        if (creep.transfer(Game.spawns["Spawn1"], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(Game.spawns["Spawn1"].pos)
+            return;
+        }
+    }
+
+    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source.pos);
+        return;
+    }
 }
