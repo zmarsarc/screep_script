6525b0956d61f12c6d608582ec6bcc49373c1ae9
zengyu <zeng.yu.1993@gmail.com>
2021-04-17 17:25:25 +0800

    [FIX] creep不工作

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,23 @@
 module.exports = {
     loop: function() {
         Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'worker');
-        for (let creep in Game.creeps) {
-            const doWork = () => this.workerMove(Game.rooms[0], creep);
-            doWork();
+        for (let room in Game.rooms) {
+            for (let creep in Game.rooms[room].find(FIND_CREEPS)) {
+                const doWork = () => this.workerMove(Game.rooms[room], creep);
+                doWork();
+            }
         }
     },
 
     workerMove: function(room: Room, creep: Creep) {
         // 如果已经装满了矿，回家卸货
-        if (creep.store.getFreeCapacity(RESOURCE_ENERGY) === 0) {
+        if (creep.store[RESOURCE_ENERGY] !== undefined && creep.store.getFreeCapacity(RESOURCE_ENERGY) === 0) {
             const spawn = room.find(FIND_MY_SPAWNS)[0];
             if (creep.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(spawn);
                 return;
             }
         }
-
 
         // 找到有能量的矿
         const sources = room.find(FIND_SOURCES_ACTIVE);
